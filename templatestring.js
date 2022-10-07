16bcb2bc607b3f8e576d066ad356b1b2147d3f35
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 23:05:48 +0530

    template string

---
+++ templatestring.js
@@ -0,0 +1,8 @@
+// template string
+let age = 23;
+let firstName = "dilip";
+
+// let about = "My name is " + firstName + " and my age is " + age;
+
+let about = `My name is ${firstName} and my age is ${age}`;
+console.log(about);
