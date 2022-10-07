504c99f0981ad6269a67da3d8cab153ebec0c369
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 22:17:09 +0530

    rules for naming variables

---
+++ rulesvariables.js
@@ -0,0 +1,21 @@
+// rules for naming variabels
+
+// you cannot start with number
+// example :
+// 1value ( invalid)
+// value1(valid)
+
+// you can use only underscore _ or dollar symbol
+// first_name(valid)
+// _firstname(valid)
+
+// first$name(valid)
+// $fisrtname(valid)
+
+// you cannot use space
+
+// first name(invalid)
+
+// convention
+
+// start with small letter and use camelCase
