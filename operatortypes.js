22049828d9274eea8b6d7c571e842541ee9c0571
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 22:58:52 +0530

    type of operator

---
+++ operatortypes.js
@@ -0,0 +1,31 @@
+// Type of operator
+
+// data types(primitive data types)
+// string " dilip"
+// number 1,2,3,4
+// booleans
+// undefined
+// null
+// BigInt
+// Symbol
+
+// let age = 22;
+// let firstName = "dilip";
+
+// console.log(typeof firstName);
+
+// convert number of string
+// console.log(typeof (age + ""));
+
+// age = age + "";
+// console.log(typeof age);
+
+// convert string to number
+
+// let myStr = +"34";
+// console.log(typeof myStr);
+
+let age = 18;
+age = String(age);
+
+console.log(typeof age);
