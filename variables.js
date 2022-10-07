1c8ca14dba734f72d0073bf96b124654f783aac4
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 22:07:52 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+"use strict";
+// intro to variables
+
+// variables can store some client Information
+// we can use that information later
+// we can change that information later
+
+// declare a variable
+
+var firstName = "Dilip Kumar";
+
+// use a variable
+
+console.log(firstName);
+
+// change value
+firstName = "dilip is a student";
+
+console.log(firstName);
