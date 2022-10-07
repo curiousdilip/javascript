9fa9a820bc1e5c3748b2320d5446cda4425ca0c9
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 22:48:32 +0530

    string methods

---
+++ stringmethods.js
@@ -0,0 +1,27 @@
+// trim() is used to remove spaces from string
+// toUpperCase()
+// toLowerCase()
+// slice
+
+// let firstName = "    Dilip   ";
+// let newName = firstName.trim();
+// console.log(newName);
+// console.log(newName.length);
+
+// firstName = firstName.trim();
+// console.log(firstName);
+// console.log(firstName.length);
+
+// let firstName = "My name is Dilip";
+// firstName = firstName.toUpperCase();
+// console.log(firstName.toUpperCase());
+
+// firstName = firstName.toLowerCase();
+
+// console.log(firstName);
+
+let firstName = "dilip";
+
+let newName = firstName.slice(0, 3);
+
+console.log(newName);
