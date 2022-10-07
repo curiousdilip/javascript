12e99862109dbb43f86ab24f9762f42970c429df
Dilip Kumar <curiousdilip@gmail.com>
2022-10-07 23:02:15 +0530

    string concatination

---
+++ stringconcat.js
@@ -0,0 +1,11 @@
+//  string concatination
+
+// let string1 = "hello";
+// let string2 = "Dilip";
+
+let string1 = "17";
+let string2 = "20";
+
+let string3 = +string1 + +string2;
+
+console.log(string3);
