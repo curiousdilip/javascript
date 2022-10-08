5d620c29220d4daecfce31dc4585cc745e5b34ee
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 15:35:45 +0530

    if else and falsey values

---
+++ ifelse.js
@@ -0,0 +1,16 @@
+// if else condition
+
+// let age = 15;
+
+// if (age > 18) {
+//   console.log("You can play football");
+// } else {
+//   console.log("You can play mario");
+// }
+
+// let num = 1;
+// if (num % 2 === 0) {
+//   console.log("number is even");
+// } else {
+//   console.log("number is odd");
+// }
---
+++ truefalseval.js
@@ -0,0 +1,20 @@
+// truthy and falsey values
+
+// falsy values
+// false
+// ""
+// null
+// undefined
+// 0
+
+// truthy
+// "abc"
+// 1,-1
+
+let firstName = "dilip";
+
+if (firstName) {
+  console.log(firstName);
+} else {
+  console.log("empty");
+}
