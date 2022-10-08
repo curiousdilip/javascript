0872792b19008d1cea99942f996f740c44a22322
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 22:28:46 +0530

    break and continue

---
+++ breakcontinue.js
@@ -0,0 +1,18 @@
+// break keyword
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+// console.log("out of the loop");
+
+//continue
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  console.log(i);
+}
+console.log("out of the loop");
