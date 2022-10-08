08ba8ed8380fb7cfae3f398d844d83424c2f55d7
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 15:24:36 +0530

    boolean and comparison operator

--- booleancomp.js
+++ booleancomp.js
@@ -1,1 +1,20 @@
 //  Boolean and Comparison operator
+
+// Booleans - true, false
+
+// let num1 = 1;
+// let num2 = 0;
+// console.log(num1 > num2);
+
+// == vs ===
+// === compares datatypes as well
+// == compares value
+
+// let n = 7;
+// let m = 7;
+// console.log(n === m);
+
+// != vs !==
+let n = 7;
+let m = 9;
+console.log(n != m);
