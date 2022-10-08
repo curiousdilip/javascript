bcbbe7291a228255bd11efccd500781cc74fbb63
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 22:00:19 +0530

    while loop example

---
+++ whileloopeg.js
@@ -0,0 +1,31 @@
+// while loop example
+let num = 10;
+let total = 0;
+
+// total = total + 1;
+// total = total + 1;
+// total = total + 1;
+// total = total + 1;
+
+// total = 0 + 1;
+// total = 1 + 2;
+// total = 3 + 3;
+// total = 6 + 4;
+// total = 10 + 5;
+// total = 15 + 6;
+// total = 21 + 7;
+// total = 28 + 8;
+// total = 36 + 9;
+// total = 45 + 10;
+
+// console.log(total);
+
+// let i = 0;
+// while (i <= 10) {
+//   total = total + i;
+//   i++;
+//   console.log(total);
+// }
+let n = 10;
+let total2 = (n * (n + 1)) / 2; // math formula to find sum of n nautral numbers
+console.log(total2);
