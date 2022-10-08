43d87e460b27748fe0adbc5c8bff6ef5e5b6ea6a
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 16:08:49 +0530

    if else if

---
+++ ifelseif.js
@@ -0,0 +1,21 @@
+// if
+// else if
+// else if
+// else
+
+let tempdegree = 17;
+if (tempdegree < 0) {
+  console.log("extremely cold outside");
+} else if (tempdegree < 16) {
+  console.log("it is cold outside");
+} else if (tempdegree < 25) {
+  console.log("weather is okay");
+} else if (tempdegree < 35) {
+  console.log("lets go for swim");
+} else if (tempdegree < 45) {
+  console.log("turn on AC");
+} else {
+  console.log("too hot");
+}
+
+// if one condition is satisfied we will not test for the other conditions
