60f125e91c88365ad6b9302c68df5fb38915ab81
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 16:39:56 +0530

    while loop

---
+++ whileloop.js
@@ -0,0 +1,43 @@
+// while Loop
+
+// 0 to 9
+
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+// DRY -- Don't repeat yourself
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
