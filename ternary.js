563d94ebe77d09b057024530e78a5ae2069e3f32
Dilip Kumar <curiousdilip@gmail.com>
2022-10-08 15:39:23 +0530

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,17 @@
+// Ternary operator
+// let age = 1;
+// let drink;
+
+// if (age >= 5) {
+//   drink = "coffee";
+// } else {
+//   drink = "milk";
+// }
+
+// console.log(drink);
+
+// Ternary operator/ conditional operator
+
+let age = 4;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
