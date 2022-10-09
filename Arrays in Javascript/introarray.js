b5f2beafd1aa2a6edfa6b15ed05b5c76f7025ecb
Dilip Kumar <curiousdilip@gmail.com>
2022-10-09 20:49:25 +0530

    intro to arrays

---
+++ Arrays in Javascript/introarray.js
@@ -0,0 +1,22 @@
+// intro to arrays -- collection of elements, ordered collection of items,can store any types of datatypes
+// reference type also know as objects
+
+//how to create arrays
+
+// let fruits = ["apple", "banana", "orange", "pineapple", "grapes"];
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(fruits[0], numbers);
+
+let fruits = ["apple", "banana", "orange", "pineapple", "grapes"];
+let obj = {}; //object literals
+
+// fruits[1] = "seb";
+// console.log(fruits);
+
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
+
+console.log(typeof fruits);
+console.log(typeof obj);
+
+//array indexing
