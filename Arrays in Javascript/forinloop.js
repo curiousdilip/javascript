a9fef52c86c6aedcea7641acd781901e19aeb49b
Dilip Kumar <curiousdilip@gmail.com>
2022-10-09 23:42:49 +0530

    for in loop

---
+++ Arrays in Javascript/forinloop.js
@@ -0,0 +1,5 @@
+// for in loop in array---it iterates index
+const fruits = ["apple", "mango", "grapes", "banana"];
+for (let index in fruits) {
+  console.log(fruits[index]);
+}
--- Arrays in Javascript/forofloop.js
+++ Arrays in Javascript/forofloop.js
@@ -3,3 +3,7 @@ const fruits = ["apple", "mango", "grapes", "banana"];
 for (let fruit of fruits) {
   console.log(fruit);
 }
+
+// for (i = 0; fruits.length > i; i++) {----------------------------same loop
+//   console.log(fruits[i]);
+// }
