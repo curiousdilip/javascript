3e36f506e89897cbef449750567d3c35a49e8394
Dilip Kumar <curiousdilip@gmail.com>
2022-10-09 23:26:03 +0530

    for loop for array

--- Arrays in Javascript/forlooparray.js
+++ Arrays in Javascript/forlooparray.js
@@ -2,6 +2,10 @@
 
 let fruits = ["apple", "mango", "grapes", "banana"];
 lengtharray = fruits.length - 1;
+let fruits2 = [];
 for (i = 0; i <= lengtharray; i++) {
-  console.log(fruits[i]);
+  //   console.log(fruits[i].toUpperCase());
+  fruits2.push(fruits[i]);
 }
+
+console.log(fruits2);
