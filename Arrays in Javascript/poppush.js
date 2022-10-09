6af182cefb4c7e98d00d33b70114e2c54ceb2aef
Dilip Kumar <curiousdilip@gmail.com>
2022-10-09 20:58:19 +0530

    push pop, shift unshift

---
+++ Arrays in Javascript/poppush.js
@@ -0,0 +1,22 @@
+// array push pop is faster than shift & unshift
+
+// array shift unshift
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits);
+
+// push
+// fruits.push("grapes");
+// console.log(fruits);
+
+//pop
+// let poppedFruit = fruits.pop();
+// console.log(fruits);
+// console.log(poppedFruit);
+
+// shift
+fruits.shift("grapes");
+console.log(fruits);
+
+// unshift
+fruits.unshift("pineapple");
+console.log(fruits);
