754f9f526adaa775b6816b498e9810260104b16b
Dilip Kumar <curiousdilip@gmail.com>
2022-10-09 23:32:01 +0530

    creating array using const

---
+++ Arrays in Javascript/constarray.js
@@ -0,0 +1,8 @@
+// use const for creating array
+
+// heap memory["apple", "mango", "grapes", "banana"] -- 0x11
+const fruits = ["apple", "mango", "grapes", "banana"]; // address is stored --0x11
+fruits.push("banana");
+console.log(fruits);
+
+// use const when creating reference data type
