86e641feab5eeb9ade286e6aa90cf7abfa224eb3
Dilip Kumar <curiousdilip@gmail.com>
2022-10-10 10:50:52 +0530

    computed properties

---
+++ Objects in JavaScript/computed.js
@@ -0,0 +1,27 @@
+// computer properties
+
+const key1 = "objectkey1";
+const key2 = "objectkey2";
+const key3 = "objectkey3";
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+const value3 = "myvalue3";
+
+// const obj = {
+//   objectkey1: "myvalue1",
+//   objectkey2: "myvalue2",
+// };
+
+// const obj = {
+//   [key1]: value1,
+//   [key2]: value2,
+//   [key3]: value3,
+// };
+
+const obj = {};
+obj[key1] = value1;
+obj[key2] = value2;
+obj[key3] = value3;
+
+console.log(obj);
