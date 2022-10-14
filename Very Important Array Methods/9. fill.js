1e850a07f7ddca6ee98151d12252a2ad01477e31
Dilip Kumar <curiousdilip@gmail.com>
2022-10-14 23:41:27 +0530

    fill Method

---
+++ Very Important Array Methods/9. fill.js
@@ -0,0 +1,10 @@
+// fill method
+// value, start,end
+// [-1, -1, -1];
+
+// const myArray = new Array(10).fill(-1);
+// console.log(myArray);
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
+myArray.fill(0, 2, 6); //fill will change original array
+console.log(myArray);
