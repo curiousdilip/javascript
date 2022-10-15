8ed125beda8f7dbb81b12b572b3814c21644fec1
Dilip Kumar <curiousdilip@gmail.com>
2022-10-15 13:10:48 +0530

    iterable

--- More Useful Things/1. iterables.js
+++ More Useful Things/1. iterables.js
@@ -2,22 +2,27 @@
 // jispe hum for of loop laga saken
 // string, array are iterable
 
-const firstName = "Dilip";
+// const firstName = "Dilip";
 
-for (char of firstName) {
-  console.log(char);
-}
+// for (char of firstName) {
+//   console.log(char);
+// }
 
-const items = ["item1", "item2", "item3"];
-for (let item of items) {
-  console.log(item);
-}
+// const items = ["item1", "item2", "item3"];
+// for (let item of items) {
+//   console.log(item);
+// }
 
 // const users = { key1: "value1", key2: "value2" }; //object are not iterables
 // for (let user of users) {
 //   console.log(user);
 // }
+
 // array like object
 // jinke pas length property hoti hai
 // aur jinko hum index se access kar sakte hai
 // example : - string
+
+const firstName = "Dilip";
+console.log(firstName.length);
+console.log(firstName[2]);
