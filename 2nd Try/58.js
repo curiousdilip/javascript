d3ebe7d0c6e60438c1064abcf408de9c2ee2e23e
Dilip Kumar <curiousdilip@gmail.com>
2022-12-26 23:37:45 +0530

    filter method

---
+++ 2nd Try/58.js
@@ -0,0 +1,15 @@
+// filter method
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const oddeven = numbers.filter((number) => {
+  return number % 2 === 0;
+});
+
+console.log(oddeven);
+
+const oddno = numbers.filter((number) => {
+  return number % 2 !== 0;
+});
+
+console.log(oddno);
