43218627868184a63807b2a306151204d8bdbbed
Dilip Kumar <curiousdilip@gmail.com>
2022-12-27 23:53:30 +0530

    fill method

---
+++ 2nd Try/64.js
@@ -0,0 +1,10 @@
+//  Fill Method
+
+// value, start, end
+
+const myArray = new Array(10).fill(0);
+console.log(myArray);
+
+const myArray2 = [1, 2, 3, 4, 5, 6, 7, 8];
+myArray2.fill(0, 4, 6);
+console.log(myArray2);
