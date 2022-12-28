2e934ae0f1f8d3dff267f24f621fb59a30a7eea5
Dilip Kumar <curiousdilip@gmail.com>
2022-12-28 23:34:02 +0530

    optional chaing

---
+++ 2nd Try/70.js
@@ -0,0 +1,11 @@
+// optional Chaining
+
+const user = {
+  firstName: "Dilip",
+  //   address: { house: 13 },
+};
+
+// let user;
+
+console.log(user?.firstName);
+console.log(user?.address?.house);
