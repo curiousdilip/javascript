d4c3993018e2090860e81cd532a53bcd718f80ed
Dilip Kumar <curiousdilip@gmail.com>
2022-12-31 23:51:24 +0530

    getter setter

---
+++ 2nd Try/90.js
@@ -0,0 +1,33 @@
+// getter and setters
+class person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.age = age;
+    this.lastName = lastName;
+  }
+
+  get fullName() {
+    //get keyword help to use method as property
+    return `Your Fullname is ${this.firstName} ${this.lastName}`;
+  }
+
+  set fullName(fullName) {
+    const [firstName, lastName] = fullName.split("");
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const person1 = new person("Dilip", "Kumar", 23);
+console.log(person1);
+console.log(person1.fullName);
+console.log(person1.firstName);
+console.log(person1.lastName);
+// person1.firstName = "Vishal"; //change name
+// person1.lastName = "Singh";
+
+person1.fullName = "Kumar Ramesh";
+console.log(person1);
+
+console.log(person1.firstName);
+console.log(person1.lastName);
