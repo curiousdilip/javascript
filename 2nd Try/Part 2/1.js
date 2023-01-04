e113b108b703566d7a905d8a92d7fa79b06ceb34
Dilip Kumar~git <curiousdilip@gmail.com>
2023-01-04 21:15:47 +0530

    how javacsript Works overview

---
+++ 2nd Try/Part 2/1.js
@@ -0,0 +1,32 @@
+// how javacsript Works overview
+
+
+// compilation
+// code execution
+
+// why compilation
+
+// How Javscript Code Executes
+
+
+// what is global execution context?
+// what is local execution context?
+// closures
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName="Dilip";
+var number=23;
+
+
+// 1. compile
+// - tokanizing/lexing  --> code is broken in to small parts known as tokan
+// - parsing ---> those broken parts are understood and AST(abtract Syntax tree) is formed
+// - code generation --> with the help of AST the code is created
+
+// ECMAscript
+// 1. Early Error Checking
+// 2. determing appropriate Scope for variables
+
+
+// 2. code execution
