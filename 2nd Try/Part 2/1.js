814b87a0d59bb4ab948178f4bab488f8be7f1a01
Dilip Kumar <curiousdilip@gmail.com>
2023-01-09 22:18:40 +0530

    how to execute javascript code

--- 2nd Try/Part 2/1.js
+++ 2nd Try/Part 2/1.js
@@ -1,6 +1,5 @@
 // how javacsript Works overview
 
-
 // compilation
 // code execution
 
@@ -8,18 +7,16 @@
 
 // How Javscript Code Executes
 
-
 // what is global execution context?
 // what is local execution context?
 // closures
 console.log(this);
 console.log(window);
 console.log(firstName);
-var firstName="Dilip";
-var number=23;
+var firstName = "Dilip";
+var number = 23;
 
-
-// 1. compile
+// 1. compilation phase
 // - tokanizing/lexing  --> code is broken in to small parts known as tokan
 // - parsing ---> those broken parts are understood and AST(abtract Syntax tree) is formed
 // - code generation --> with the help of AST the code is created
@@ -28,5 +25,6 @@ var number=23;
 // 1. Early Error Checking
 // 2. determing appropriate Scope for variables
 
-
-// 2. code execution
+// 2. code execution phase
+//    in js code executes insides execution context
+// GLOBAL EXECUTION CONTEXT
---
+++ 2nd Try/Part 2/2.js
@@ -0,0 +1,11 @@
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "Dilip";
+var number = 23;
+
+// 1. Global Execution Context
+// -- Creation Phase
+// -- Code execution phase
+
+// Javscript is syncronous programming language
