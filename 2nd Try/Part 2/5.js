a902aa0e989ec78abd54f388dd966b2bbd7a4ff8
Dilip Kumar <curiousdilip@gmail.com>
2023-01-09 23:09:14 +0530

    what happens to function expression

---
+++ 2nd Try/Part 2/5.js
@@ -0,0 +1,8 @@
+// function expression
+var myFunc = function () {
+  console.log("this is my function");
+};
+
+myFunc();
+
+// function expression is treated as variable that's it gives undefined just like any other variable
