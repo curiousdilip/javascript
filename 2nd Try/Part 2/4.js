39c5d9acbf7d81fdac457c13a8d7dfcb57d38cca
Dilip Kumar <curiousdilip@gmail.com>
2023-01-09 23:02:51 +0530

    hoisting

---
+++ 2nd Try/Part 2/4.js
@@ -0,0 +1,15 @@
+// hoisting
+console.log(this);
+console.log(window);
+console.assert(myFunction);
+myFunction();
+console.log(fullName);
+
+function myFunction() {
+  console.log("this is my function");
+}
+
+var firstName = "Dilip";
+var lastName = "Kumar";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
