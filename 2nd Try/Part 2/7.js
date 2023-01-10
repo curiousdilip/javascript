448bac25d855358d68cab19a5e18c5d31f002f11
Dilip Kumar <curiousdilip@gmail.com>
2023-01-10 22:52:04 +0530

    quick summary

---
+++ 2nd Try/Part 2/7.js
@@ -0,0 +1,27 @@
+// Quick summary
+
+// Compilation
+// 1. error
+// 2. Scope
+
+// Code execution - in order to execute JS Code js creates execution context
+// 1.creation phase - memory create
+// 2. line by line code execution
+
+console.log("Hello World");
+let firstName = "Dilip";
+let lastName = "Kumar";
+
+const myFunc = function () {
+  let var1 = "first Variable";
+  let var2 = "second Variable";
+  console.log(var1);
+  console.log(var2);
+};
+
+// no error
+
+// global scope
+// fname
+// lname
+// function
