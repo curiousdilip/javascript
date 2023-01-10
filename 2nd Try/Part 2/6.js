d53da30d75353177e45b17b83b2f95925e615fd3
Dilip Kumar <curiousdilip@gmail.com>
2023-01-10 22:39:58 +0530

    are let and const ar hoisted

---
+++ 2nd Try/Part 2/6.js
@@ -0,0 +1,15 @@
+// uncaught reference error
+// cannot access 'firstname' before initialization
+console.log(firstName);
+let firstName = "Dilip";
+
+console.log(firstName);
+
+// code execution
+// GEC-
+//  creation - global memory is created
+//  code execution
+
+// ---
+
+// TDZ - Temporal Dead Zone
