e1bb628f57d2a096ee1c1b37db4c35f636302c31
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 14:08:20 +0530

    string indexing

---
+++ 3rd Try/Part 1/6-string-indexing.js
@@ -0,0 +1,16 @@
+// String indexing
+
+let firstName = "Dilip"
+
+// D i l i p
+// 0 1 2 3 4
+
+console.log(firstName[0])
+// length of String
+// firstName.length
+
+console.log((firstName.length))
+
+// last index : length -1
+
+console.log(firstName[firstName.length - 1])
