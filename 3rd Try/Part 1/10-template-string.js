93b45b1409926e90060013d4b505c9c39277dc21
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 14:52:05 +0530

    template string

---
+++ 3rd Try/Part 1/10-template-string.js
@@ -0,0 +1,8 @@
+// Template string
+
+let age = 24;
+let name = "Dilip Kumar"
+let abtme = "My name is " + name + " and I am " + age + " years old "
+let hello = `Hello, My name is ${name} and I am ${age} years old`
+console.log(hello)
+console.log(abtme)
