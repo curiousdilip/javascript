980e83f5e5fb3eac2b930378885021afe5100e1d
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 13:43:01 +0530

    rules of creating variables

---
+++ 3rd Try/Part 1/3-rules-for-variables.js
@@ -0,0 +1,21 @@
+// rules for naming varibles
+
+// you cannnot start with a number 
+// example
+// 1value (invalid)
+// value1( valid)
+
+var value1 = 1300
+console.log(value1 / 12)
+// you can only use _ underscore  or $ dollar dymbol
+// first_name (valid)
+// _firstname (valid)
+
+// first$name (valid)
+// $firstname(valid)
+
+// you cannot use spaces
+var first_name = "Dilip Kumar" //snake case writing
+var firstName = "Dilip Kumar" // camelcase writing
+// convention
+// start with a small letter and use camelCase
