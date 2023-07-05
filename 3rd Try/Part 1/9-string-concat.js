af75f4333d1a4fd236620430f3eac3ea52f4dfae
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 14:47:37 +0530

    string concat

---
+++ 3rd Try/Part 1/9-string-concat.js
@@ -0,0 +1,11 @@
+// string concatenation
+
+let firstName = "Dilip"
+let lastName = "Kumar"
+let fullName = firstName + " " + lastName
+console.log(fullName)
+
+let str1 = "12"
+let str2 = "34"
+let newStr = +str1 + +str2
+console.log(newStr)
