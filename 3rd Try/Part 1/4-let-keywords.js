f0b2a1b49fb7efb614f2cac45bf9bc565524f4f5
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 13:51:11 +0530

    let keyword

---
+++ 3rd Try/Part 1/4-let-keywords.js
@@ -0,0 +1,11 @@
+// let keyword
+// declare variable with let keyword
+let firstName = "Dilip"
+// console.log(firstName)
+firstName = "Vishal"
+
+console.log(firstName)
+
+
+
+// Block scope vs function scope (discussed later)
