21c5235bbb352f25a141cfdc08587563ee1fc623
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 13:28:28 +0530

    Create 2-variables.js

---
+++ 3rd Try/Part 1/2-variables.js
@@ -0,0 +1,19 @@
+// Intro to variables
+// "use strict" - to check errors
+// Varibles can store some information
+// we can use that information later
+//  we can change that information
+
+
+// Declare  a variable
+
+var firstName = "Dilip";
+
+// use a variable 
+console.log(firstName);
+
+// Change a variable
+
+firstName = "Vishal"
+
+console.log(firstName);
