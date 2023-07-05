5695012168a7f439553d61a7c4522f8624c6c66d
Dilip Kumar <curiousdilip@gmail.com>
2023-07-05 14:40:41 +0530

    string and typeof

---
+++ 3rd Try/Part 1/7-string-methods.js
@@ -0,0 +1,15 @@
+// trim()-- will give me a new string with removed spaces
+// toUpperCase()
+// toLowerCase()
+// slice
+
+// string are immutabale, methods will not change strings
+
+// let firstName = "    Dilip    "
+let firstName = "Dilip"
+console.log(firstName.length)
+console.log(firstName.trim()) // Dilip
+console.log(firstName.trim().length)
+console.log(firstName.toUpperCase()) // DILIP
+console.log(firstName.toLowerCase()) // dilip
+console.log(firstName.slice(0, 3))//Dil
---
+++ 3rd Try/Part 1/8-typeof-operator.js
@@ -0,0 +1,41 @@
+// typeof operator
+
+
+// datatypes(primitive datatypes)
+// string - "hello","Dilip"
+// number - 1,2,3,4
+// booleans - true false
+// undefined - 
+// null
+// BigInt
+// Symbol
+
+let age = 24;
+let first$name = "Dilip "
+
+console.log(typeof first$name)
+console.log(typeof true)
+console.log(typeof age)
+console.log("-----------------")
+
+// convert number to string
+
+age = age + ""; // 22 -> "22"
+console.log(typeof age)
+console.log("-----------------")
+let lotNo = 231
+
+console.log(typeof lotNo)
+
+lotNo = String(lotNo)
+console.log(typeof lotNo)
+
+
+console.log("-----------------")
+// convert string to number
+
+let myStr = "34"
+console.log(typeof myStr)
+
+myStr = +"34"
+console.log(typeof myStr)
