56a7a0aa3f932d873cf9e35b610eaf48b0b4e562
Dilip Kumar <curiousdilip@gmail.com>
2023-07-06 14:34:02 +0530

    booleans

---
+++ 3rd Try/Part 1/12-booleon-comparision.js
@@ -0,0 +1,23 @@
+// booleans and comparision operator
+
+//true or false - > boooleans
+let a = 6
+let b = 45
+console.log(a >= b)
+console.log("-----============--------")
+
+// == & === 
+
+let c = "6"
+let d = 6
+console.log(c == d)// check only values
+console.log(c === d)// check datatypes too 
+console.log("-----============--------")
+
+
+// != vs !==
+
+let e = "6"
+let f = 6
+console.log(e !== f)
+console.log(e != f)
