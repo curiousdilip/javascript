efb53f3aad540ee8990acd49428b8f898fd0a428
Dilip Kumar <curiousdilip@gmail.com>
2023-07-06 18:21:50 +0530

    if-else-if

--- 3rd Try/Part 1/18-if-else-if.js
+++ 3rd Try/Part 1/18-if-else-if.js
@@ -2,3 +2,20 @@
 // else if 
 // else if
 // else
+
+let tempInDegree = -1;
+
+if (tempInDegree < 0) {
+    console.log("extremely cold outside")
+} else if (tempInDegree < 16) {
+    console.log("it is cold outside")
+} else if (tempInDegree < 25) {
+    console.log("weather is okay")
+} else if (tempInDegree < 35) {
+    console.log("let's go for swim")
+} else if (tempInDegree < 45) {
+    console.log("turn on ac")
+} else {
+    console.log("too hot")
+}
+
