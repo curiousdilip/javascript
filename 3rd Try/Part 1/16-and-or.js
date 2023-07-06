d6076c8d7c68e8b8035d3b76b028cd4cc296d06e
Dilip Kumar <curiousdilip@gmail.com>
2023-07-06 16:16:47 +0530

    and & or operator

---
+++ 3rd Try/Part 1/16-and-or.js
@@ -0,0 +1,26 @@
+// || & or operator
+
+let firstName = "Dilip"
+let age = 24
+
+// if (firstName[0] === "D") {
+//     console.log(`Your Name starts with ${firstName[0]}`)
+// }
+
+// if (age > 18) {
+//     console.log(`you are above 18`)
+// }
+
+// AND Operator
+// if (firstName[0] === "D" && age > 18) {//if both condition are true then the condtion is true else its false
+//     console.log(`Your Name starts with ${firstName[0]} and your age is above 18`)
+
+// } else
+//     console.log(`don't know`)
+
+// OR Operator 
+if (firstName[0] === "D" || age > 18) {//if one condition is true then whole condition is true
+    console.log(`Your Name starts with ${firstName[0]} and your age is above 18`)
+
+} else
+    console.log(`don't know`)
