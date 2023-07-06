1fdd49a39450fcee1dc337432e1e7e4ad91fc114
Dilip Kumar <curiousdilip@gmail.com>
2023-07-06 18:53:06 +0530

    while loop

---
+++ 3rd Try/Part 1/20-while-loop.js
@@ -0,0 +1,24 @@
+// while loop
+
+//  DRY - > Don't repeat your self
+
+
+// print 1 to 10
+
+let i = 1
+
+
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+console.log("================")
+
+
+// Print 2 table
+
+let j = 2;
+while (j <= 20) {
+    console.log(j)
+    j = j + 2
+}
