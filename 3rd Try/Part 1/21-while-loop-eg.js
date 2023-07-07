b1dcb7921aafff5f135bf3ea7edd70b0e30fec2c
Dilip Kumar <curiousdilip@gmail.com>
2023-07-07 23:40:10 +0530

    while loop examples

---
+++ 3rd Try/Part 1/21-while-loop-eg.js
@@ -0,0 +1,14 @@
+// while loop examples
+
+// let num = 10
+// let total = 0
+// let i = 1
+
+// while (i <= 10) {
+//     total = total + i
+//     i++
+
+// }
+// console.log(total)
+
+console.log(10 * (10 + 1) / 2) // same result for sum of natural numbers
