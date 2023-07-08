9cc0ae720762008c34dc65d8afaa7680fed54fd9
Dilip Kumar <curiousdilip@gmail.com>
2023-07-08 23:33:50 +0530

    while loop in array

--- 3rd Try/Part 1/31-const-array.js
+++ 3rd Try/Part 1/31-const-array.js
@@ -1,6 +1,6 @@
 // use const for creating array
 
-// and main array is stored in heap memory -->["apple", "orange", "grapes"]
+// and main array is stored in heap memory -->["apple", "orange", "grapes"] 
 
 const fruits = ["apple", "orange", "grapes"]//fruit name ke variable me ek address stored hai
 fruits.push("banana")
---
+++ 3rd Try/Part 1/32-while-loop-array.js
@@ -0,0 +1,7 @@
+// while loop in array
+const fruits = ["apple", "orange", "grapes"]
+let i = 0
+while (i < fruits.length) {
+    console.log(fruits[i].toUpperCase())
+    i++
+}
