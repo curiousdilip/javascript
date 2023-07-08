201c4edf4272bae1795b198d20f5ccf8bcccc808
Dilip Kumar <curiousdilip@gmail.com>
2023-07-09 00:22:05 +0530

    array destructuring

---
+++ 3rd Try/Part 1/35-array-destructuring.js
@@ -0,0 +1,15 @@
+// array destructuring
+
+const array1 = ["value1", "value2", "value3", "value4"]
+
+// let myvar1 = array1[0]
+// let myvar2 = array1[1]
+
+// console.log(myvar1, myvar2)
+
+let [myvar1, myvar2, ...array2] = array1
+
+// let array2 = array1.slice(2)
+
+console.log(myvar1, myvar2)
+console.log(array2)
