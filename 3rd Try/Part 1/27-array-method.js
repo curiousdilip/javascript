c6b6077b07f1c05f5ffdec1f11d82d1ea3624aa2
Dilip Kumar <curiousdilip@gmail.com>
2023-07-08 14:14:28 +0530

    array methods

---
+++ 3rd Try/Part 1/27-array-method.js
@@ -0,0 +1,30 @@
+// array  push pop
+
+// array shift unshift
+
+let fruits = ["apple ", "mango", "grapes"]
+console.log(fruits)
+
+// push
+
+fruits.push("banana")
+console.log(fruits)
+
+// pop -> remove the item from the last of the array
+console.log(fruits.pop())
+
+console.log(fruits)
+
+// unshift -> add items at the start
+
+fruits.unshift("orange")
+console.log(fruits)
+
+// shift - it will remove item from start
+
+console.log(fruits.shift())
+console.log(fruits)
+
+
+// push and pop is fast as compared to shift and unshift
+
