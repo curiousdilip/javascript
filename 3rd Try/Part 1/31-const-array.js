ee6c090ee1e0201c19c5fcf31b7a2ad083f80811
Dilip Kumar <curiousdilip@gmail.com>
2023-07-08 23:29:45 +0530

    const in array

---
+++ 3rd Try/Part 1/31-const-array.js
@@ -0,0 +1,7 @@
+// use const for creating array
+
+// and main array is stored in heap memory -->["apple", "orange", "grapes"]
+
+const fruits = ["apple", "orange", "grapes"]//fruit name ke variable me ek address stored hai
+fruits.push("banana")
+console.log(fruits)
