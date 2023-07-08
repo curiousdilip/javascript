3d2d1a1b6cd99cf60d98718526b84a47e02da749
Dilip Kumar <curiousdilip@gmail.com>
2023-07-09 00:36:07 +0530

    object intro

---
+++ 3rd Try/Part 1/36-object-intro.js
@@ -0,0 +1,14 @@
+// objects---> reference data types
+// arraysa are good but not sufficient
+// for real world data
+// object store key value pairs
+// object don't have index
+
+//  how to create objects
+const person = { age: 24, name: "Dilip", address: "E-13, Qutub vihar, ND-71", hobbies: ["guitar", "sleeping", "music"] } // object literals
+
+console.log(typeof person)
+console.log(person.hobbies)
+//  how to access data from objects
+
+//  how to add key data from objects
