a737fe9407e04a494e10f522ef4806bb6d202dd3
Dilip Kumar <curiousdilip@gmail.com>
2023-07-09 09:56:01 +0530

    computed properties

---
+++ 3rd Try/Part 1/39-computed-properties.js
@@ -0,0 +1,21 @@
+// computed properties
+
+const key1 = "objkey1"
+const key2 = "objkey2"
+
+const value1 = "myval1"
+const value2 = "myval2"
+
+// const obj = {
+//     [key1]: value1,
+//     [key2]: value2,
+// }
+
+
+const obj = {
+
+}
+obj[key1] = value1
+obj[key2] = value2
+
+console.log(obj)
